3a465f1256da6bc5ed318b0682a9d98b7db09af3
Sajjad Shaikh <samaralishaikh212@gmail.com>
2022-12-15 21:46:54 +0530

    smart contract

--- components/Navbar.js
+++ components/Navbar.js
@@ -90,7 +90,7 @@ const Navbar = () => {
 
         <div
           className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${
-            !toggleDrawer ? "-translate-y-[100vh]" : "translate-y-0"
+            !toggleDrawer ? "-translate-y-[200vh]" : "translate-y-0"
           } transition-all duration-700`}
         >
           <ul className="mb-4">
