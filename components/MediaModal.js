acd4bd35142c75de632075e3f02b56d22b46aa63
sajjad <samaralishaikh212@gmail.com>
2023-01-08 20:54:00 +0530

    bug fix

--- components/MediaModal.js
+++ components/MediaModal.js
@@ -126,7 +126,7 @@ export default function MediaModal({
                         Copy link
                       </button>
                       <button
-                        onClick={()=>handleClick(name, link)}
+                        onClick={()=>handleClick(name, src)}
                         class="w-full flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
                         href="#"
                       >
